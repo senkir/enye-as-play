b4430f3355b567de04914340f31c89b69e79cd13
Travis Castillo <tcastillo@crowdcompass.com>
2015-03-30 22:15:45 -0700

    trying to get button to work

--- app/controllers/SeedController.scala
+++ app/controllers/SeedController.scala
@@ -7,6 +7,7 @@ import play.api.db.slick._
 import scala.util.Random
 import play.api.mvc.Flash
 import java.sql.Timestamp
+import play.api.Logger
 
 object SeedController extends Controller {
 
@@ -16,13 +17,15 @@ object SeedController extends Controller {
 
   def apply = DBAction { implicit request =>
     val rand = new Random()
-    val count = Model.platforms.list.count()
+    val count = Model.platforms.list.size
     if (rand.nextBoolean() || count > 0) {
+      Logger.debug(s"seed apply success")
       // Models.platforms += Platform(None, "Android", None, None)
       // Models.platforms += Platform(None, "iOS", None, None)
-      Redirect(routes.SeedController.ask()).flashing(Flash() + "success" -> "done!")
+      Redirect(routes.SeedController.ask()).flashing("success" -> "done!")
     } else {
-      Redirect(routes.SeedController.ask()).flashing(Flash() + ("error" -> "seed data has already been applied!"))
+      Logger.debug(s"seed apply faliure")
+      Redirect(routes.SeedController.ask()).flashing("error" -> "seed data has already been applied!")
     }
   }
 }
