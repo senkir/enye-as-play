f55421f6ca884ece03156d4aad41fc60bf4399ce
Travis Castillo <tcastillo@crowdcompass.com>
2015-04-20 09:29:12 -0700

    setting up styles to skip polymer

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,13 +7,18 @@ import scala.slick.lifted.TableQuery
 import play.api.db.slick.Config.driver.simple._
 import play.api.db.slick._
 import play.api.Play.current
+import java.sql.Timestamp
 
 
 object Application extends Controller {
 
   def home = DBAction { implicit rs =>
     val app = Model.products.list
-    Ok(views.html.index("Enye Interactive",app))
+    var stamp = new Timestamp(System.currentTimeMillis)
+    val featured = Product(None,"Kelvim", 0, "1.0.2", 
+      Some("Cupcake ipsum dolor. Sit amet ice cream wafer pudding marshmallow tiramisu jujubes. Jujubes cotton candy cotton candy tart sweet roll. Pastry sweet chocolate cake bonbon bonbon pie. Croissant marzipan cookie carrot cake fruitcake marshmallow. Chocolate dragée gummies chocolate cake icing gingerbread powder biscuit. Powder candy ice cream. Donut ice cream pudding cake muffin marshmallow pastry topping. \t Liquorice jelly chocolate bar sugar plum gingerbread sugar plum jelly-o. Brownie jelly-o gingerbread caramels. Sugar plum powder bonbon. Lollipop gummi bears bear claw candy chocolate sweet macaroon. Tootsie roll pastry lemon drops. Ice cream pie jelly beans sesame snaps pie marshmallow cupcake jelly beans."), None, None)
+
+    Ok(views.html.index("Enye Interactive",featured, app))
   }
 
 
