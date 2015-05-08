8516ff7dab659ddb6c80307429d7be6d52962fe1
Travis Castillo <tcastillo@crowdcompass.com>
2015-05-07 19:21:38 -0700

    everything is basically working

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -16,7 +16,7 @@ object Application extends Controller {
     val app = Model.products.list
     var stamp = new Timestamp(System.currentTimeMillis)
     val featured = Product(None,"Kelvim", 0, "1.0.2", 
-      Some("Cupcake ipsum dolor. Sit amet ice cream wafer pudding marshmallow tiramisu jujubes. Jujubes cotton candy cotton candy tart sweet roll. Pastry sweet chocolate cake bonbon bonbon pie. Croissant marzipan cookie carrot cake fruitcake marshmallow. Chocolate dragée gummies chocolate cake icing gingerbread powder biscuit. Powder candy ice cream. Donut ice cream pudding cake muffin marshmallow pastry topping. \t Liquorice jelly chocolate bar sugar plum gingerbread sugar plum jelly-o. Brownie jelly-o gingerbread caramels. Sugar plum powder bonbon. Lollipop gummi bears bear claw candy chocolate sweet macaroon. Tootsie roll pastry lemon drops. Ice cream pie jelly beans sesame snaps pie marshmallow cupcake jelly beans."), "https://play.google.com/store/apps/details?id=com.spidermuffin.kelvim", None, None)
+      Some("Kelvim is an easy way to show the weather in <a href=”http://en.wikipedia.org/wiki/Kelvin”>Kelvin</a>.  Featuring friendly weather indicators and color coding to help you transition to a Kelvin world.  Designed in collaboration with <a href=http://ramiro.mx>Ramiro Franco</a> with weather data provided by <a href=http://www.wunderground.com/>Weather Undeground</a>.  "), "https://play.google.com/store/apps/details?id=com.spidermuffin.kelvim", None, None)
 
     Ok(views.html.index("Enye Interactive",featured, app))
   }
